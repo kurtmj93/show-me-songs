1521fc0a58ed67400f47f39f96c214d33ff88125
elizabeth189 <elizabethlopez0108@gmail.com>
2022-12-15 21:47:44 -0500

    added trailwind css to index

--- script.js
+++ script.js
@@ -3,4 +3,45 @@
 let searchbarEl = $('#search-bar');
 let searchresultsEl = $('#search-results');
 
-console.log('Hello world!');
+// YOUTUBE API CONSTANTS
+const youtubeKey = 'AIzaSyBwJo04Cg2kxSrYJ9gVy4S-3JQqDIHkqZ8';
+const searchURL = 'https://youtube.googleapis.com/youtube/v3/search'
+
+// YOUTUBE SEARCH API REQUEST OPTIONS
+let options = {
+    q: 'Dua Lipa',
+    part: 'snippet',
+    key: youtubeKey,
+    maxResults: 10,
+    type: 'video',
+    videoCategoryId: 10
+}
+
+function sendRequest() {
+    $.getJSON(searchURL, options, function(data){
+        console.log(data);
+        requestLoop(data);
+    })
+  }
+
+  function requestLoop(data) {
+
+    $.each(data.items, function(i, item){
+        var vidThumb = item.snippet.thumbnails.medium.url;
+        var vidTitle = item.snippet.title;
+        var vidDesc = item.snippet.description.substring(0, 200);
+        var vidId = item.id.videoId;
+
+        $('#search-results').append(`
+            <article class="video" data-key"${vidId}">
+            <img src="${vidThumb}" class="thumb">
+            <div class="info">
+            <h3>${vidTitle}</h3>
+            <p>${vidDesc}</p>
+            </div>
+            </article>
+        `);
+    });
+  }
+
+  $('#show').click(sendRequest);
