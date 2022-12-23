5a2e76fc54fae6d5a4ce07a7ef15fcb9b0f0f1db
jmod333 <116365950+jmod333@users.noreply.github.com>
2022-12-22 19:40:47 -0500

    Added event listener and searchSong function

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ let searchresultsEl = $('#search-results');
 const youtubeKey = 'AIzaSyBwJo04Cg2kxSrYJ9gVy4S-3JQqDIHkqZ8';
 const searchURL = 'https://youtube.googleapis.com/youtube/v3/search'
 
+// MusixMatch API Constants
+const musixMatchKey = '4bf14d6e04db8041bfd1bcad66b37a84'
+const getURL = 'https://api.musixmatch.com/ws/1.1/'
+
 // YOUTUBE SEARCH API REQUEST OPTIONS
 let options = {
     q: '',
@@ -51,18 +55,30 @@ function sendRequest(event) {
 
   $('#songtitle').submit(sendRequest);
 
-// MusixMatch API Constants
-const musixMatchKey = '4bf14d6e04db8041bfd1bcad66b37a84'
-const getURL = 'https://api.musixmatch.com/ws/1.1/'
 
 // MusixMatch lyrics search
 $.ajax({
     url: getURL,
     key: musixMatchKey,
     method: 'GET',
+    q_lyrics: '',
   }).then(function (response) {
     console.log('Ajax Reponse \n-------------');
     console.log(response);
   });
 
-  
+// Search bar event listener
+
+var searcher = document.querySelector('#search-bar')
+
+  searcher.addEventListener('submit', function() {
+    event.preventDefault();
+    searchValue =searcher.value;
+});
+
+async function searchSong(searchValue){
+    const searchResult = await fetch(`${url}/suggest/${sea}`)
+    const data = await searchResult.json();
+    
+    showData(data)
+}
