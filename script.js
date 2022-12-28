3bba063b088bb3ba190d1965f9d1a1bf8e0bf04d
elizabeth189 <elizabethlopez0108@gmail.com>
2022-12-28 08:16:31 -0500

    added CSS styling to result videos, small changes to readme

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ let searchresultsEl = $('#search-results');
 const youtubeKey = 'AIzaSyBwJo04Cg2kxSrYJ9gVy4S-3JQqDIHkqZ8';
 const searchURL = 'https://youtube.googleapis.com/youtube/v3/search'
 
+// MusixMatch API Constants
+const musixMatchKey = '4bf14d6e04db8041bfd1bcad66b37a84'
+const getURL = 'https://api.musixmatch.com/ws/1.1/'
+
 // YOUTUBE SEARCH API REQUEST OPTIONS
 let options = {
     q: '',
@@ -51,22 +55,31 @@ function sendRequest(event) {
 
   $('#songtitle').submit(sendRequest);
 
-  // MusixMatch API Constants
-const musixMatchKey = '4bf14d6e04db8041bfd1bcad66b37a84'
-const getURL = 'https://api.musixmatch.com/ws/1.1/'
-
+  
 // MusixMatch lyrics search
 $.ajax({
-    url: getURL,
-    key: musixMatchKey,
-    method: 'GET',
-  }).then(function (response) {
-    console.log('Ajax Reponse \n-------------');
-    console.log(response);
-  });
+  url: getURL,
+  key: musixMatchKey,
+  method: 'GET',
+  q_lyrics: '',
+}).then(function (response) {
+  console.log('Ajax Reponse \n-------------');
+  console.log(response);
+});
 
-  //proof of concept
-  //implement to run at the same time as the youtube api?
-  //can be simple, doesnt need to be so hard
-  //proof on concept that we can get data back and do something with the data
-  //
+// Search bar event listener
+
+var searcher = document.querySelector('#search-bar')
+
+searcher.addEventListener('submit', function() {
+  event.preventDefault();
+  searchValue =searcher.value;
+});
+
+async function searchSong(searchValue){
+  const searchResult = await fetch(`${url}/suggest/${sea}`)
+  const data = await searchResult.json();
+  
+  showData(data)
+}
+  
