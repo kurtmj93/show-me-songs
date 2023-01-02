93edba52020bb94f7a78b1c7c6918c08f7abbd74
Kurtmj93 <kurtmj93@gmail.com>
2023-01-02 15:22:25 -0500

    feed fetch results to youtube search api

--- script.js
+++ script.js
@@ -4,15 +4,11 @@ let searchbarEl = $('#search-bar');
 let searchresultsEl = $('#search-results');
 
 // YOUTUBE API CONSTANTS
-const youtubeKey = 'AIzaSyBwJo04Cg2kxSrYJ9gVy4S-3JQqDIHkqZ8';
+const youtubeKey = 'AIzaSyATkg7QM97WIyQkUHHEnw1NXMx4dhDruuA';
 const searchURL = 'https://youtube.googleapis.com/youtube/v3/search'
 
-// MusixMatch API Constants
-const musixMatchKey = '4bf14d6e04db8041bfd1bcad66b37a84'
-const getURL = 'https://api.musixmatch.com/ws/1.1/'
-
 // YOUTUBE SEARCH API REQUEST OPTIONS
-let options = {
+var optionsYT = {
     q: '',
     part: 'snippet',
     key: youtubeKey,
@@ -21,61 +17,45 @@ let options = {
     videoCategoryId: 10
 }
 
-/*
-
-function sendRequest(event) {
-    event.preventDefault();
-    options.q = $('#song').val();
-    console.log(options);
-    $.getJSON(searchURL, options, function(data){
-        searchresultsEl.empty();
-        console.log(data);
-        requestLoop(data);
-    })
-  }
-
-  function requestLoop(data) {
-
-    $.each(data.items, function(i, item){
-        var vidThumb = item.snippet.thumbnails.medium.url;
-        var vidTitle = item.snippet.title;
-        var vidDesc = item.snippet.description.substring(0, 200);
-        var vidId = item.id.videoId;
-
-        searchresultsEl.append(`
-            <article class="video" data-key="${vidId}">
-            <a href="https://www.youtube.com/watch?v=${vidId}">
-            <img src="${vidThumb}" class="thumb">
-            <div class="info">
-            <h3>${vidTitle}</h3></a>
-            <p>${vidDesc}</p>
-            </div>
-            </article>
-        `);
-    });
-  }
-
-*/
-
 // Lyrics Search Fetch URL
 
 const chartLyricsURL = 'http://api.chartlyrics.com/apiv1.asmx/SearchLyricText?lyricText=';
 
 function sendRequest(event) {
   event.preventDefault();
+  // encode text entry as HTML to append to chartLyricsURL 
   var searchVal = encodeURIComponent($('#song').val());
   var searchFetch = chartLyricsURL + searchVal;
+  // create playlist array to store results
   var playlist = [];
+  searchresultsEl.empty();
   $.ajax({
     url: searchFetch,
     dataType: "xml",
     success: function(xml) {
+      // for each SearchLyricResult returned, push string of title and into playlist array
       $('SearchLyricResult', xml).each(function(){
         var songResult = $('Song', this).text() + " by " + $('Artist', this).text();
         playlist.push(songResult);
-      })
-    },
-  });
+      });
+      for (i=0; i<5; i++) {
+          optionsYT.q = playlist[i];
+          $.getJSON(searchURL, optionsYT, function(data){
+              var vidThumb = data.items[0].snippet.thumbnails.medium.url;
+              var vidTitle = data.items[0].snippet.title;
+              var vidId = data.items[0].id.videoId;
+              console.log(vidTitle);
+              searchresultsEl.append(`
+                  <article class="video" data-key="${vidId}">
+                  <a href="https://www.youtube.com/watch?v=${vidId}">
+                  <img src="${vidThumb}" class="thumb">
+                  <div class="info">
+                  <h3>${vidTitle}</h3></a>
+                  </div>
+                  </article>
+              `);});
+      };
+  },});
 };
 
 $('#songtitle').submit(sendRequest);
